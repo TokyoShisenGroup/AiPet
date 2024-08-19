57b197a5e757172892c6c06fe8221548af216a96
cliecy <guanjiezou@gmail.com>
2024-08-19 15:32:28 +0900

    first commit

---
+++ lib/MyPage/mypage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class mypage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Hello"),
+      ),
+    );
+  }
+  
+}
